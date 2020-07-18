86acfa2c76413bcb9b30b915a3c8e5e7866f9a8b
Sreekaran <sreekaran.srinath@gmail.com>
2020-07-18 13:55:05 +0530

    Update autocommit.py - minor semantic fix in print statement

--- autocommit.py
+++ autocommit.py
@@ -16,7 +16,7 @@ while 1:
         sp.run('git add .', shell = True)
         sp.run(f'git commit --date "{str(datetime(2020, 8, 23, 13, 9, 18, 442081) + timedelta(daydelta))}"  -m {str(list(set(newlist) - set(filelist)))}')
         sp.run('git push')
-        print(f'Added, committed, and pushed file {str(list(set(newlist) - set(filelist)))} with commit datetime {str(datetime.now() + timedelta(daydelta))}')
+        print(f'Added, committed, and pushed file {str(list(set(newlist) - set(filelist)))} with commit datetime {str(datetime(2020, 8, 23, 13, 9, 18, 442081) + timedelta(daydelta))}')
         daydelta += 1
     filelist = newlist
     time.sleep(1)
