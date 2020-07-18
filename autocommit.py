ec512309cbb215f65374e14f9d03f8334fe841bd
Sreekaran <sreekaran.srinath@gmail.com>
2020-07-18 21:08:05 +0530

    ['golf-code.js']

--- autocommit.py
+++ autocommit.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 ls = sp.run("dir", shell = True, capture_output = True)
 filelist = re.findall(r'[0-9]{2}/[0-9]{2}/[0-9]{4}  [0-9]{2}:[0-9]{2} .*? [0-9]+ (\S*)', ls.stdout.decode())
 daydelta = 1
-pickUpFrom = datetime(2020, 11, 20, 13, 9, 18, 442081)
+pickUpFrom = datetime(2020, 11, 26, 13, 9, 18, 442081)
 
 while 1:
     ls = sp.run("dir", shell = True, capture_output = True)
