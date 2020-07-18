68e67d2323186d0dc3f4810f9bf119b0176fe6d4
Sreekaran <sreekaran.srinath@gmail.com>
2020-07-18 13:48:20 +0530

    Update autocommit script to push as well

--- autocommit.py
+++ autocommit.py
@@ -1,23 +1,24 @@
-import subprocess
+import subprocess as sp
 import os
 import sys
 import re
 import time
 from datetime import datetime, timedelta
 
-ls = subprocess.run("dir", shell = True, capture_output = True)
+ls = sp.run("dir", shell = True, capture_output = True)
 filelist = re.findall(r'[0-9]{2}/[0-9]{2}/[0-9]{4}  [0-9]{2}:[0-9]{2} .*? [0-9]+ (\S*)', ls.stdout.decode())
 daydelta = 1
 
 while 1:
-    ls = subprocess.run("dir", shell = True, capture_output = True)
+    ls = sp.run("dir", shell = True, capture_output = True)
     newlist = re.findall(r'[0-9]{2}/[0-9]{2}/[0-9]{4}  [0-9]{2}:[0-9]{2} .*? [0-9]+ (\S*)', ls.stdout.decode())
     if newlist != filelist:
-        subprocess.run('git add .', shell = True)
-        subprocess.run(f'git commit --date "{str(datetime.now() + timedelta(daydelta))}"  -m {str(list(set(newlist) - set(filelist)))}')
-        print(f'Added and committed file {str(list(set(newlist) - set(filelist)))} with commit datetime {str(datetime.now() + timedelta(daydelta))}')
+        sp.run('git add .', shell = True)
+        sp.run(f'git commit --date "{str(datetime(2020, 8, 23, 13, 9, 18, 442081) + timedelta(daydelta))}"  -m {str(list(set(newlist) - set(filelist)))}')
+        sp.run('git push')
+        print(f'Added, committed, and pushed file {str(list(set(newlist) - set(filelist)))} with commit datetime {str(datetime.now() + timedelta(daydelta))}')
         daydelta += 1
     filelist = newlist
-    time.sleep(10)
+    time.sleep(1)
 
 
