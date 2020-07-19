10c43001183951b7df399e0dc0d3c03da579588f
Sreekaran <sreekaran.srinath@gmail.com>
2020-07-19 12:41:37 +0530

    ['initializing-variables-with-the-assignment-operator.txt']

--- autocommit.py
+++ autocommit.py
@@ -3,18 +3,21 @@ import os
 import sys
 import re
 import time
+from datetime import datetime, timedelta
 
 ls = subprocess.run("dir", shell = True, capture_output = True)
 filelist = re.findall(r'[0-9]{2}/[0-9]{2}/[0-9]{4}  [0-9]{2}:[0-9]{2} .*? [0-9]+ (\S*)', ls.stdout.decode())
+daydelta = 1
 
 while 1:
     ls = subprocess.run("dir", shell = True, capture_output = True)
     newlist = re.findall(r'[0-9]{2}/[0-9]{2}/[0-9]{4}  [0-9]{2}:[0-9]{2} .*? [0-9]+ (\S*)', ls.stdout.decode())
     if newlist != filelist:
         subprocess.run('git add .', shell = True)
-        subprocess.run(f'git commit -m {str(list(set(newlist) - set(filelist)))}')
-        print(f'Added and committed file {str(list(set(newlist) - set(filelist)))}')
+        subprocess.run(f'git commit --date "{str(datetime.now() + timedelta(daydelta))}"  -m {str(list(set(newlist) - set(filelist)))}')
+        print(f'Added and committed file {str(list(set(newlist) - set(filelist)))} with commit datetime {str(datetime.now() + timedelta(daydelta))}')
+        daydelta += 1
     filelist = newlist
-    time.sleep(60)
+    time.sleep(10)
 
 
