ecd58028aacb2bb82e5ae2633bff56e254640ed3
Sreekaran <sreekaran.srinath@gmail.com>
2020-09-12 13:09:18 +0530

    ['use-conditional-logic-with-if-statements.js']

--- autocommit.py
+++ autocommit.py
@@ -14,7 +14,7 @@ while 1:
     newlist = re.findall(r'[0-9]{2}/[0-9]{2}/[0-9]{4}  [0-9]{2}:[0-9]{2} .*? [0-9]+ (\S*)', ls.stdout.decode())
     if newlist != filelist:
         sp.run('git add .', shell = True)
-        sp.run(f'git commit --date "{str(datetime(2020, 8, 23, 13, 9, 18, 442081) + timedelta(daydelta))}"  -m {str(list(set(newlist) - set(filelist)))}')
+        sp.run(f'git commit --date "{str(datetime(2020, 9, 11, 13, 9, 18, 442081) + timedelta(daydelta))}"  -m {str(list(set(newlist) - set(filelist)))}')
         sp.run('git push')
         print(f'Added, committed, and pushed file {str(list(set(newlist) - set(filelist)))} with commit datetime {str(datetime(2020, 8, 23, 13, 9, 18, 442081) + timedelta(daydelta))}')
         daydelta += 1
