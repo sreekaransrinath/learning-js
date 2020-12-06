6be5e582ebfb1e802f3347dd60be7ac278f26ec8
Sreekaran <sreekaran.srinath@gmail.com>
2020-12-07 00:00:00 +0530

    []

--- autocommit.py
+++ autocommit.py
@@ -2,13 +2,20 @@ import subprocess as sp
 import re
 import time
 from datetime import datetime, timedelta
+from random import randint
+
+def genRandomTime():
+    hour = random.randint(0, 24)
+    minute = random.randint(0, 60)
+    second = random.randint(0, 60)
+    return hour, minute, second
 
 ls = sp.run("dir", shell = True, capture_output = True)
 filelist = re.findall(r'[0-9]{2}/[0-9]{2}/[0-9]{4}  [0-9]{2}:[0-9]{2} .*? [0-9]+ (\S*)', ls.stdout.decode())
 daydelta = 1
-pickUpFrom = datetime(2020, 12, 5)
 
 while 1:
+    pickUpFrom = datetime(2020, 12, 5, genRandomTime())
     ls = sp.run("dir", shell = True, capture_output = True)
     newlist = re.findall(r'[0-9]{2}/[0-9]{2}/[0-9]{4}  [0-9]{2}:[0-9]{2} .*? [0-9]+ (\S*)', ls.stdout.decode())
     if newlist != filelist:
