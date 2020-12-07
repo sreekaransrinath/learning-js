dcb0d5cd236fb278958aaf6652efa8fe0a2c1d28
Sreekaran <sreekaran.srinath@gmail.com>
2020-12-07 18:25:40 +0530

    '['accessing-object-properties-with-variables.js']'

--- autocommit.py
+++ autocommit.py
@@ -4,18 +4,12 @@ import time
 from datetime import datetime, timedelta
 from random import randint
 
-def genRandomTime():
-    hour = random.randint(0, 24)
-    minute = random.randint(0, 60)
-    second = random.randint(0, 60)
-    return hour, minute, second
-
 ls = sp.run("dir", shell = True, capture_output = True)
 filelist = re.findall(r'[0-9]{2}/[0-9]{2}/[0-9]{4}  [0-9]{2}:[0-9]{2} .*? [0-9]+ (\S*)', ls.stdout.decode())
 daydelta = 1
 
 while 1:
-    pickUpFrom = datetime(2020, 12, 5, genRandomTime())
+    pickUpFrom = datetime(2020, 12, 5, randint(0, 24), randint(0, 60), randint(0, 60))
     ls = sp.run("dir", shell = True, capture_output = True)
     newlist = re.findall(r'[0-9]{2}/[0-9]{2}/[0-9]{4}  [0-9]{2}:[0-9]{2} .*? [0-9]+ (\S*)', ls.stdout.decode())
     if newlist != filelist:
