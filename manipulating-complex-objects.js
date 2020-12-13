ff14b6fd9f843a497a4837066203e14b79e4f556
Sreekaran <sreekaran.srinath@gmail.com>
2020-12-13 23:07:29 +0530

    ['manipulating-complex-objects.js']

---
+++ manipulating-complex-objects.js
@@ -0,0 +1,25 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Trashmen",
+    "title": "Surfin Bird",
+    "release_year": 1964,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+  // Add a record here
+];
